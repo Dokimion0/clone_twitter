eabe9ef72ac1a00041415676cde652d1a2c9fba9
LCY36 <leecy3610@gmail.com>
2022-08-12 14:45:55 +0900

    update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "style.css";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter base="/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
 );
 
 
